7ed4ff8de7490ee1667ec4fcbbb9cb169e52b788
megaIt202 <megait202@gmailcom>
2024-03-21 18:46:20 +0900

    [김인규]  - sns 중복확인 기능 정리

---
+++ src/main/java/com/marondal/marondalgram/user/UserRestController.java
@@ -0,0 +1,58 @@
+package com.marondal.marondalgram.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marondal.marondalgram.user.service.UserService;
+
+@RequestMapping("/user")
+@RestController
+public class UserRestController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/join")
+	public Map<String, String> join(
+			@RequestParam("loginId") String loginId
+			, @RequestParam("password") String password
+			, @RequestParam("name") String name
+			, @RequestParam("email") String email) {
+		
+		
+		int count = userService.addUser(loginId, password, name, email);
+		
+		Map<String, String> resultMap = new HashMap<>();
+		
+		if(count == 1) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+		
+	}
+	
+	@GetMapping("/duplicate-id")
+	public Map<String, Boolean> isDuplicateId(
+			@RequestParam("loginId") String loginId) {
+		
+		boolean isDuplicate = userService.isDuplicateId(loginId);
+		
+		Map<String, Boolean> resultMap = new HashMap<>();
+		
+		resultMap.put("isDuplicate", isDuplicate);
+		
+		return resultMap;
+		
+	}
+
+}
---
+++ src/main/java/com/marondal/marondalgram/user/service/UserService.java
@@ -0,0 +1,33 @@
+package com.marondal.marondalgram.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.marondalgram.common.EncryptUtils;
+import com.marondal.marondalgram.user.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public int addUser(String loginId, String password, String name, String email) {
+		
+		String encryptPassword = EncryptUtils.md5(password);
+		return userRepository.insertUser(loginId, encryptPassword, name, email);
+		
+	}
+	
+	
+	public boolean isDuplicateId(String loginId) {
+		int count = userRepository.selectCountByLoginId(loginId);
+		
+		if(count >= 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
